8f17df08be0d5f7477c01c2e25b6d788f8f10cac
crocdialer <crocdialer@googlemail.com>
2015-07-30 12:27:52 +0200

    looking nice, performance could be better

--- energy_pipe.ino
+++ energy_pipe.ino
@@ -1,4 +1,5 @@
 #include <FastLED.h>
+#include <colorpalettes.h>
 #include <fab_utils.h>
 
 /////////////////////// GLOBAL DEFINITIONS ////////////////////////
@@ -6,11 +7,11 @@
 // LED definitions
 #define LED_TYPE WS2812
 #define COLOR_ORDER GRB
-#define NUM_LEDS 60 
+#define NUM_LEDS 240 
 #define LED_PIN 6 
 
 // update rate in Hz
-#define UPDATE_RATE 60 
+#define UPDATE_RATE 120 
 
 ///////////////////////////////////////////////////////////////////
 
@@ -24,14 +25,15 @@ class EnergyPipe
 {
  public:
 
-    static const int MAX_NUM_PARTICLES = 25;
+    static const int MAX_NUM_PARTICLES = 5;
 
     EnergyPipe(uint32_t num_leds, uint32_t led_pin):
     m_num_leds(num_leds),
     m_fade(.75f),
     m_damping(.5f),
     m_min_life(10.f),
-    m_max_life(10.f)
+    m_max_life(10.f),
+    m_fade_out_life(2.f)
     {
         m_leds = new CRGB[num_leds]();
         FastLED.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(m_leds, m_num_leds);
@@ -44,7 +46,7 @@ class EnergyPipe
 
     void update(float time_delta)
     {
-        // decide if new particles should be spawned
+        //TODO: decide if new particles should be spawned here !?
         
         float damp_factor = 1.f - m_damping * time_delta;
 
@@ -61,18 +63,36 @@ class EnergyPipe
             p.velocity *= damp_factor; 
 
             // update position
+            p.last_position = p.position;
             p.position += p.velocity * time_delta;
-
-            // update colour
-            //p.colour = 
 
             // update life time
             p.life_time -= time_delta;
+
+            // remove particles already beyond limits
+            if(p.position > 2.f){ p.life_time = 0.f; }
+
+            p.fade_out_factor = clamp<float>(p.life_time, 0.f, m_fade_out_life) / m_fade_out_life;
+
+            // determine range of movement 
+            //int index_from, index_to;
+            //index_from = p.last_position * (m_num_leds - 1);
+            //index_to = p.position * (m_num_leds - 1);
+
+            //// swap indices
+            //if(index_from > index_to)
+            //{
+            //  int tmp = index_from;
+            //  index_from = index_to;
+            //  index_to = tmp;
+            //}
         }
 
         // update LED array
         for(int i = 0; i < m_num_leds; i++)
         {
+            float led_pos = i / (float)(m_num_leds - 1);
+
             // set interpolated colour value
             for(int j = 0; j < MAX_NUM_PARTICLES; j++)
             {
@@ -82,23 +102,21 @@ class EnergyPipe
                 if(p.life_time <= 0.f){ continue; }
                 
                 // distance falloff
-                float distance_factor = clamp<float>(abs(p.position - (i / (float)(m_num_leds - 1))), 0.f, 1.f);
-                float thresh = .025f; 
-                distance_factor = distance_factor > thresh ? 0 : distance_factor;
+                float distance = p.position - led_pos;
+                const float thresh = .01f;
+                float distance_factor = abs(distance) > thresh ? 0 : abs(distance) / thresh;
 
-                float tmp = distance_factor / abs(distance_factor - thresh);
                 CRGB color = p.color;
-                color.r *= tmp;
-                color.g *= tmp;
-                color.b *= tmp;
+                color.r *= distance_factor * p.fade_out_factor;
+                color.g *= distance_factor * p.fade_out_factor;
+                color.b *= distance_factor * p.fade_out_factor;
                 m_leds[i] += color;
             }
 
             // fade all LEDS down
-            float fade = m_fade;
-            m_leds[i].r *= fade;
-            m_leds[i].g *= fade;
-            m_leds[i].b *= fade;
+            m_leds[i].r *= m_fade;
+            m_leds[i].g *= m_fade;
+            m_leds[i].b *= m_fade;
         }
         FastLED.show();
     }
@@ -113,7 +131,7 @@ class EnergyPipe
             if(p.life_time <= 0.f)
             { 
                 p.life_time = random<float>(m_min_life, m_max_life); 
-                p.position = - .25f;
+                p.last_position = p.position = 0.f;
                 p.velocity = the_velocity;
                 p.color = the_color;
                 break; 
@@ -127,26 +145,37 @@ class EnergyPipe
         m_max_life = the_max;
     }
 
+    void set_fade_out_life(const float the_life){ m_fade_out_life = the_life; }
+
+    void set_damping(float the_damping){ m_damping = the_damping; }
+
+    void set_fade(float the_fade){ m_fade = clamp<float>(the_fade, 0.f, 1.f); }
+
  private:
     
-    // use e.g. a parabolic formula here
     inline float accel(const float position) const 
     {
-      // x^2
-      return 1.f * (1.f - 2.f * position); 
+      // f(x) = ax^4 + bx^2, f'(x) = 4ax^3 + 2bx
+      const float a = -1.f, b = 2.f;
+      float x_1 = 2 * (position - 0.5f);
+      float ret = 4.f * a * x_1 * x_1 * x_1 + 2 * b * x_1;
+      return -ret;  
     }
 
     struct Particle
     {
         CRGB color;
+        float last_position;
         float position;
         float velocity;
         float life_time;
+        float fade_out_factor;
 
         Particle(): 
         position(0.f),
         velocity(0.f),
         life_time(1.f),
+        fade_out_factor(1.f),
         color(CRGB::Red)
         {}
     };
@@ -159,6 +188,9 @@ class EnergyPipe
     
     // min / max life time of particles
     float m_min_life, m_max_life;
+
+    // max lifetime without fade-out applied
+    float m_fade_out_life;
 
     // particle objects
     Particle m_particles[MAX_NUM_PARTICLES];
@@ -194,6 +226,11 @@ void setup()
 {
   // init serial
   // init SPI
+
+  // pipe settings
+  g_pipe.set_life_time(12.f, 20.f);
+  g_pipe.set_fade_out_life(5.f);
+  g_pipe.set_damping(.6f);
 }
 
 ///////////////////////////////////////////////////////////////////
@@ -209,11 +246,12 @@ void loop()
     // emit particle
     if(g_emit_counter <= 0)
     {
-        g_emit_counter = random<int>(5000, 10000);
-        float p_vel = random<float>(0.f, .06f);
-        CRGB color = CRGB(random<int>(0, 255),
-                          random<int>(0, 255),
-                          random<int>(100, 255));
+        g_emit_counter = random<int>(1000, 3000);
+        float p_vel = random<float>(.2f, .8f);
+        CRGB color = g_indicator ? CRGB::Orange : CRGB::Blue;
+        //CRGB color = CRGB(random<int>(0, 255),
+        //                  random<int>(0, 255),
+        //                  random<int>(100, 255));
 
         g_pipe.emit_particle(color, p_vel);
     }
